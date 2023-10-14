fbd1d59e7ce7f1b6490a3185ed7e8c73644e0ee3
nassirou <nassirou9993@gmail.com>
2023-10-14 22:55:02 +0100

    the navbar mobile photography

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Logo from '../img/header/logo.svg';
 import MobileNav from './MobileNav';
+import Socials from './Socials'
 
 import {Link} from 'react-router-dom'
 
@@ -9,17 +10,23 @@ const Header = () => {
     <header className='bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex  items-center'>
       <div className='flex flex-col lg:flex-row lg:items-center w-full justify-between'>
 
-        <Link to={'/'}>
+        <Link to={'/'} className='max-w-[200px]'>
           <img src={Logo} alt='' />
         </Link>
         {/**nav initially hidden show on desktop mode */}
-        <nav className='hidden lg:flex gap-x-10'>
+        <nav className='hidden lg:flex gap-x-10 font-semibold'>
           <Link to={'/'} className='text-[#696c6d] hover:text-primary transition'>Home</Link>
           <Link to={'/about'} className='text-[#696c6d] hover:text-primary transition'>About</Link>
           <Link to={'/portefolio'} className='text-[#696c6d] hover:text-primary transition'>Portfolio</Link>
           <Link to={'/contact'} className='text-[#696c6d] hover:text-primary transition'>Contact</Link>
         </nav>
+
+      
       </div>
+        {/**social mode */}
+        <Socials />
+
+        <MobileNav />
     </header>
   )
 };
--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {IoMdClose} from 'react-icons/io';
+import {CgMenuRight} from 'react-icons/cg';
+
+import { Link } from 'react-router-dom';
+import {motion} from 'framer-motion';
+
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+
+  const [openMenu, setOpenMenu] = useState(false)
+  return (<nav className='text-primary xl:hidden'>
+    <div className='text-3xl cursor-pointer '>
+      <CgMenuRight />
+    </div>
+    <div className='bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20'>
+      <div className='text-4xl absolute z-30 left-4 top-14 text-primary cursor-pointer'>
+        <IoMdClose />
+      </div>
+      <ul className='h-full flex flex-col justify-center items-center gap-y-8  text-primary font-primary font-bold text-3xl'>
+        <li>
+          <Link to='/'>Home</Link> 
+        </li>
+        <li>
+          <Link to='/about'>About</Link> 
+        </li>
+        <li>
+          <Link to='/portefolio'>Portfolio</Link> 
+        </li>
+        <li>
+          <Link to='/contact'>Contact</Link> 
+        </li>
+      </ul>
+    </div>
+  </nav>
+  );
 };
 
 export default MobileNav;
